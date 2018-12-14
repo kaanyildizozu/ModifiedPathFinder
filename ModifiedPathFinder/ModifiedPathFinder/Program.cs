f8723928f709ea211ba7ee9b48c8b7675351dabc
ky <kaan.yildiz@ozu.edu.tr>
2018-12-15 00:44:26 +0300

    At this state, it can create a binary tree given a string with "( )" notation. It also prints all the possible paths from root to leaves.

--- ModifiedPathFinder/ModifiedPathFinder/Program.cs
+++ ModifiedPathFinder/ModifiedPathFinder/Program.cs
@@ -9,12 +9,12 @@ namespace BinaryTreeTraverser
 {
     public class Node
     {
-        public int data;
+        public string data;
         public Node left, right;
 
-        public Node(int item)
+        public Node(string value)
         {
-            data = item;
+            data = value;
             left = right = null;
         }
     }
@@ -25,11 +25,11 @@ namespace BinaryTreeTraverser
 
         public virtual void PrintPaths(Node node)
         {
-            int[] path = new int[100];
+            string[] path = new string[100];
             PrintPathsRecur(node, path, 0);
         }
 
-        public virtual void PrintPathsRecur(Node node, int[] path, int pathLen)
+        public virtual void PrintPathsRecur(Node node, string[] path, int pathLen)
         {
             if (node == null)
                 return;
@@ -46,7 +46,7 @@ namespace BinaryTreeTraverser
             }
         }
 
-        public virtual void printArray(int[] ints, int len)
+        public virtual void printArray(string[] ints, int len)
         {
             for (int i = 0; i < len; i++)
                 Console.Write(ints[i] + " ");
@@ -93,28 +93,30 @@ namespace BinaryTreeTraverser
                 return null;
 
             int index = -1;
+            int value_digits = 0;
             Node root;
 
-            if (str[si + 1] != '(' && str[si + 1] != ')' && si < (str.Length - 1))
+            while (str[si + value_digits] != '(' && str[si + value_digits] != ')')
+                value_digits++;
+
+            if ((str[si + value_digits] == '(' || str[si + value_digits] == ')') && si < (str.Length - 1))
             {
+                string digits = str.Substring(si, value_digits);
+                root = string.IsNullOrEmpty(digits) ? new Node("") : new Node(digits);
 
-                char[] chars = { str[si], str[si + 1] };
-                string digits = new string(chars);
-                root = new Node(System.Convert.ToInt32(digits));
-
-                if (si + 2 <= ei && str[si + 2] == '(')
-                    index = FindIndex(str, si + 2, ei);
+                if (si + value_digits + 1 <= ei && str[si + value_digits] == '(')
+                    index = FindIndex(str, si + value_digits, ei);
 
                 if (index != -1)
                 {
-                    root.left = TreeFromString(str, si + 3, index - 1);
+                    root.left = TreeFromString(str, si + value_digits + 1, index - 1);
                     root.right = TreeFromString(str, index + 2, ei - 1);
                 }
             }
             else
             {
 
-                root = new Node(str[si] - '0');
+                root = new Node(str[si].ToString());
 
                 if (si + 1 <= ei && str[si + 1] == '(')
                     index = FindIndex(str, si + 1, ei);
@@ -136,17 +138,11 @@ namespace BinaryTreeTraverser
         static void Main(string[] args)
         {
             BinaryTree tree = new BinaryTree();
-            //tree.root = new Node(10);
-            //tree.root.left = new Node(8);
-            //tree.root.right = new Node(2);
-            //tree.root.left.left = new Node(3);
-            //tree.root.left.right = new Node(5);
-            //tree.root.right.left = new Node(2);
-            //tree.PrintPaths(tree.root);
 
-            string str = "1(3(5(6(4(10(11)())())(7(8(12)())()))())())(4(7(6(5(6)())(7(6)()))())())";
+            string str = "10(3(5(6222(4(10(11)())())(7(8(12)())()))())())(4(7(6(5(6)())(7(6)()))())())";
             Node root = tree.TreeFromString(str, 0, str.Length - 1);
-            tree.PreOrder(root);
+            tree.PrintPaths(root);
+            //tree.PreOrder(root);
         }
     }
 }
